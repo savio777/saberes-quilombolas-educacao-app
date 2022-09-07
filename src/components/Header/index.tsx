48fbcac966d45a5ec27b4bf212abfc354a7ef2e8
Sávio <saviopf22@gmail.com>
2022-09-07 16:10:20 -0300

    botão no header ir para perfil

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {useNavigation} from '@react-navigation/native';
 
 import PersonEllipseMockupSvg from '../../assets/person-ellipse-mockup.svg';
 import {Container, ContainerFlex, Content, Title} from './styles';
@@ -10,26 +11,30 @@ interface Props {
   title: string;
 }
 
-const Header: React.FC<Props> = ({title}) => (
-  <Container>
-    <Content>
-      <ContainerFlex principal>
-        <Title numberOfLines={2}>{title}</Title>
-      </ContainerFlex>
-      <ContainerFlex>
-        <TouchableOpacity>
-          <MaterialCommunityIcons
-            name="magnify"
-            size={38}
-            color={colors.black}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <PersonEllipseMockupSvg width={38} height={38} />
-        </TouchableOpacity>
-      </ContainerFlex>
-    </Content>
-  </Container>
-);
+const Header: React.FC<Props> = ({title}) => {
+  const {navigate} = useNavigation();
+
+  return (
+    <Container>
+      <Content>
+        <ContainerFlex principal>
+          <Title numberOfLines={2}>{title}</Title>
+        </ContainerFlex>
+        <ContainerFlex>
+          <TouchableOpacity>
+            <MaterialCommunityIcons
+              name="magnify"
+              size={38}
+              color={colors.black}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigate('Profile')}>
+            <PersonEllipseMockupSvg width={38} height={38} />
+          </TouchableOpacity>
+        </ContainerFlex>
+      </Content>
+    </Container>
+  );
+};
 
 export default Header;
