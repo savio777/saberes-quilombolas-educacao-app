6c3e3dce49f85eb32c1e8c5224d8dbea0db4c4d1
Sávio <saviopf22@gmail.com>
2023-02-28 21:59:34 -0300

    remoção da lib de input mask

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -14,6 +14,13 @@ export const maskPattern = (valueString = '', patternString = '') => {
   return patternString.replace(/#/g, () => valueString[i++] || '');
 };
 
+export const maskDate = (value = '') =>
+  value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '$1/$2')
+    .replace(/(\d{2})(\d)/, '$1/$2')
+    .replace(/(\d{4})(\d)/, '$1');
+
 export const textLoremIpsum =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
 
--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { useNavigation } from "@react-navigation/native";
+import React, {useState, useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { mask } from "react-native-text-input-mask";
 
-import { InputBorder, Button } from "../../components";
-import { Title, SubTitle, ViewFlex, ViewRow } from "./styles";
-import ContainerKeyboardAvoidingView from "../../components/ContainerKeyboardAvoidingView";
-import { Content } from "../../components/ContainerKeyboardAvoidingView/styles";
+import {InputBorder, Button} from '../../components';
+import {Title, SubTitle, ViewFlex, ViewRow} from './styles';
+import ContainerKeyboardAvoidingView from '../../components/ContainerKeyboardAvoidingView';
+import {Content} from '../../components/ContainerKeyboardAvoidingView/styles';
+import {maskDate} from '../../helpers/utils';
 
 const SignUp: React.FC = () => {
-  const { goBack } = useNavigation();
+  const {goBack} = useNavigation();
 
-  const [birthday, setBirthday] = useState("");
+  const [birthday, setBirthday] = useState('');
 
   useEffect(() => {
     const changeValueBirtMask = async () => {
-      const valueMasked = await mask("[00]/[00]/[0000]", birthday, false);
+      const valueMasked = maskDate(birthday);
+
       setBirthday(valueMasked);
     };
 
@@ -39,7 +40,7 @@ const SignUp: React.FC = () => {
           <ViewFlex />
         </ViewRow>
 
-        <SubTitle>Cadastre e acesse{"\n"} todo o nosso conteudo</SubTitle>
+        <SubTitle>Cadastre e acesse{'\n'} todo o nosso conteudo</SubTitle>
         <InputBorder title="Nome" />
         <InputBorder title="E-mail" />
         <InputBorder title="Senha" secureTextEntry />
