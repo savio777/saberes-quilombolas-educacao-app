c9ac5b037ea6548d954f88f1eafc74653495d936
Sávio <saviopf22@gmail.com>
2023-06-10 19:48:45 -0300

    correção caminho pdf

--- src/screens/PartThree/index.tsx
+++ src/screens/PartThree/index.tsx
@@ -23,6 +23,41 @@ const Sequencia_didatica_01_reconhecimento = {
   uri: 'bundle-assets://pdfs/part_three/Sequencia_didatica_01_reconhecimento.pdf',
 };
 
+// parte 03
+const Caderno_de_Orientações_Pedagógicas = {
+  uri: 'bundle-assets://pdfs/matrizes_curriculares/parte_03/Caderno_de_Orientações_Pedagógicas.pdf',
+};
+const Caderno_Política_de_Educação_EScolar_Quilombola_NEQUI = {
+  uri: 'bundle-assets://pdfs/matrizes_curriculares/parte_03/Caderno_Política_de_Educação_EScolar_Quilombola_NEQUI.pdf',
+};
+const Caderno_Quilombola = {
+  uri: 'bundle-assets://pdfs/matrizes_curriculares/parte_03/Caderno_Quilombola.pdf',
+};
+const Diretrizes_Curriculares_Estaduais_para_Educação_Escolar_Quilombola = {
+  uri: 'bundle-assets://pdfs/matrizes_curriculares/parte_03/Diretrizes_Curriculares_Estaduais_para_Educação_Escolar_Quilombola.pdf',
+};
+const Matriz_Curricular_01 = {
+  uri: 'bundle-assets://pdfs/matrizes_curriculares/parte_03/Matriz_Curricular_01.pdf',
+};
+
+const Orientações = {
+  uri: 'bundle-assets://pdfs/matrizes_curriculares/parte_03/Orientações.pdf',
+};
+const proposta_quilombola = {
+  uri: 'bundle-assets://pdfs/matrizes_curriculares/parte_03/proposta_quilombola.pdf',
+};
+const Roteiro_de_aula_para_educação_quilombola = {
+  uri: 'bundle-assets://pdfs/matrizes_curriculares/parte_03/Roteiro_de_aula_para_educação_quilombola.pdf',
+};
+
+const Caderno_Pedagogico_para_a_educacao_escolar_quilombola = {
+  uri: 'bundle-assets://pdfs/part_two/Caderno_Pedagogico_para_a_educação_escolar_quilombola.pdf',
+};
+
+const Oficina_para_organizacao_do_semestre = {
+  uri: 'bundle-assets://pdfs/part_two/Caderno_Pedagogico_para_a_educacao_escolar_quilombola.pdf',
+};
+
 export default () => {
   const [selectedPdf, setSelectedPdf] = useState<Source | undefined>();
   const [selectedPdfTitle, setSelectedPdfTitle] = useState('');
@@ -90,6 +125,112 @@ export default () => {
                 openModal(
                   Sequencia_didatica_01_reconhecimento,
                   'Sequencia didatica 01 reconhecimento',
+                )
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Caderno Pedagógico para a educacao escolar quilombola"
+              text=""
+              onPress={() =>
+                openModal(
+                  Caderno_Pedagogico_para_a_educacao_escolar_quilombola,
+                  'Caderno Pedagógico para a educacao escolar quilombola',
+                )
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Oficina para organização do semestre"
+              text=""
+              onPress={() =>
+                openModal(
+                  Oficina_para_organizacao_do_semestre,
+                  'Oficina para organização do semestre',
+                )
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Caderno de Orientações Pedagógicas"
+              text=""
+              onPress={() =>
+                openModal(
+                  Caderno_de_Orientações_Pedagógicas,
+                  'Caderno de Orientações Pedagógicas',
+                )
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Caderno Quilombola"
+              text=""
+              onPress={() =>
+                openModal(Caderno_Quilombola, 'Caderno Quilombola')
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Diretrizes Curriculares Estaduais para Educação Escolar Quilombola"
+              text=""
+              onPress={() =>
+                openModal(
+                  Diretrizes_Curriculares_Estaduais_para_Educação_Escolar_Quilombola,
+                  'Diretrizes Curriculares Estaduais para Educação Escolar Quilombola',
+                )
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Caderno Política de Educação EScolar Quilombola NEQUI"
+              text=""
+              onPress={() =>
+                openModal(
+                  Caderno_Política_de_Educação_EScolar_Quilombola_NEQUI,
+                  'Caderno Política de Educação EScolar Quilombola NEQUI',
+                )
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Matriz Curricular 01"
+              text=""
+              onPress={() =>
+                openModal(Matriz_Curricular_01, 'Matriz Curricular 01')
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Orientações"
+              text=""
+              onPress={() => openModal(Orientações, 'Orientações')}
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Proposta quilombola"
+              text=""
+              onPress={() =>
+                openModal(proposta_quilombola, 'Proposta quilombola')
+              }
+            />
+
+            <BannerKnowledgeLibrary
+              marginLeft
+              title="Roteiro de aula para educação quilombola"
+              text=""
+              onPress={() =>
+                openModal(
+                  Roteiro_de_aula_para_educação_quilombola,
+                  'Roteiro de aula para educação quilombola',
                 )
               }
             />
